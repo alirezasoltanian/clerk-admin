0806e361757911b5a29a806425bba8e8214f2c12
Alireza Zare <azare242@gmail.com>
2023-12-27 14:52:32 +0330

    feat: main pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-import Image from 'next/image'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-export default function Home() {
+async function page() {
   return (
-    <main className="  ">
-    </main>
+    <div className="flex flex-wrap gap-5 items-center mt-10 justify-center ">
+      <div className="flex flex-col gap-5 border p-4 items-center justify-center">
+        <div className="bg-slate-200 aspect-square h-60" />
+        <Link
+          className={cn(buttonVariants({ variant: 'outline' }))}
+          href="/auth/admin/login"
+        >
+          LOGIN AS ADMIN
+        </Link>
+      </div>
+
+      <div className="flex flex-col gap-5 border p-4 items-center justify-center">
+        <div className="bg-slate-200 aspect-square h-60" />
+        <Link
+          className={cn(buttonVariants({ variant: 'outline' }))}
+          href="/auth/clerk/login"
+        >
+          LOGIN AS CLERK
+        </Link>
+      </div>
+    </div>
   )
 }
+
+export default page
